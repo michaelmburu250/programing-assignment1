7dd2f62e3639c4046b2495bfb55fcf867c99da3c
michaelmburu250 <michaelmburu250@gmail.com>
2025-09-18 15:16:09 +0300

    Add files via upload

---
+++ program units assignment .c
@@ -0,0 +1,22 @@
+ //simple c program 
+/*
+name:michaelMburu 
+registration CT100/G/26177/25
+description:My semister units
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    printf("Mathematics for science\n");
+    printf("Introduction to programing and algorithms\n");
+    printf("Introduction to computer applications\n");
+    printf("Essentials of accounting\n");
+    printf("communication and information literacy skills\n");
+    printf("fundamentals of ICT\n");
+    printf("Analogue electronics\n");
+    printf("Computer systems and organizations\n");
+    return 0;
+    }
+    
---
+++ program user details assignment .c
@@ -0,0 +1,26 @@
+ //simple c program 
+/*
+name:michaelMburu 
+registration CT100/G/26177/25
+description:User details 
+*/
+#include<stdio.h>//pre-processor descriptive.scanf
+
+//main function 
+int main(){
+printf("Hello:\n");
+int height;
+printf("Enter your height:");
+scanf("%d",& height);
+printf("Your height is %d");
+int phone_no;
+printf("\nEnter your phone_no:");
+scanf("%d",& phone_no);
+printf("\nYour phone_no is %d");
+int weight;
+printf("\nEnter your weight:");
+scanf("%d",& weight);
+printf("\nYour weight is %d");
+    return 0;
+    }
+    
