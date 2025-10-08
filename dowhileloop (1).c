d9b6173d49979f260f47b61960b85c1c8740db5a
michaelmburu250 <michaelmburu250@gmail.com>
2025-10-08 20:09:50 +0300

    Add files via upload

---
+++ dowhileloop (1).c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/*simple c program 
+name:michael Mburu 
+reg::CT100/G/26177/25
+description:Do_while_loop
+*/
+int main()
+{
+int password;
+int correct_password=1234;
+
+do{
+printf("Enter your password");
+scanf("%d",&password);
+}
+while(passwordl!=correct_password);
+printf("access granted\n");
+
+
+return 0;
+}
